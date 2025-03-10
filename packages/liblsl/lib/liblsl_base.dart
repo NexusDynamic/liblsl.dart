ae9daa144715a480030bfed1e40c379d4ecb974b
zeyus <dev@zeyus.com>
2025-03-10 21:54:56 +0100

    blah

--- packages/liblsl/lib/liblsl_base.dart
+++ packages/liblsl/lib/liblsl_base.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
 import 'dart:ffi';
-import 'dart:io';
+// import 'dart:io';
 import 'package:ffi/ffi.dart';
-import 'package:system_info2/system_info2.dart';
-import '../liblsl.dart';
+// import 'package:system_info2/system_info2.dart';
+import 'package:liblsl/liblsl.dart';
 
 enum LSLContentType {
   /// EEG (for Electroencephalogram)
@@ -46,13 +46,10 @@ enum LSLContentType {
 
 // need to implement full data types later
 class LSL {
-  late final Liblsl _liblsl;
   Pointer<lsl_streaminfo_struct_>? _streamInfo;
   Pointer<lsl_outlet_struct_>? _streamOutlet;
-
-  LSL() {
-    _liblsl = Liblsl(_loadLibrary());
-  }
+  final DynamicLibrary _liblsl;
+  LSL() : _liblsl = DynamicLibrary.process();
 
   Future<void> createStreamInfo(
       {String streamName = "DartLSLStream",
@@ -61,7 +58,7 @@ class LSL {
       double sampleRate = 250.0,
       lsl_channel_format_t channelFormat = lsl_channel_format_t.cft_float32,
       String sourceId = "DartLSL"}) async {
-    _streamInfo = _liblsl.lsl_create_streaminfo(
+    _streamInfo = lsl_create_streaminfo(
         streamName.toNativeUtf8() as Pointer<Char>,
         streamType.charPtr,
         channelCount,
@@ -70,46 +67,44 @@ class LSL {
         sourceId.toNativeUtf8() as Pointer<Char>);
   }
 
-  int get version => _liblsl.lsl_library_version();
+  int get version => lsl_library_version();
 
   Future<void> createOutlet({int chunkSize = 0, int maxBuffer = 1}) async {
-    _streamOutlet =
-        _liblsl.lsl_create_outlet(_streamInfo!, chunkSize, maxBuffer);
+    _streamOutlet = lsl_create_outlet(_streamInfo!, chunkSize, maxBuffer);
   }
 
   Future<void> waitForConsumer({double timeout = 60}) async {
-    final consumerFound =
-        _liblsl.lsl_wait_for_consumers(_streamOutlet!, timeout);
+    final consumerFound = lsl_wait_for_consumers(_streamOutlet!, timeout);
     if (consumerFound == 0) {
       throw TimeoutException('No consumer found within $timeout seconds');
     }
   }
 
-  DynamicLibrary _loadLibrary() {
-    if (Platform.isWindows) {
-      return DynamicLibrary.open(
-          '${Directory.current.path}/liblsl/liblsl.1.16.2-win-amd64.dll');
-    }
-    if (Platform.isMacOS) {
-      if (SysInfo.kernelArchitecture == ProcessorArchitecture.arm64) {
-        return DynamicLibrary.open(
-            '${Directory.current.path}/liblsl/liblsl.1.16.2-osx-arm64.dylib');
-      } else {
-        return DynamicLibrary.open(
-            '${Directory.current.path}/liblsl/liblsl.1.16.2-osx-amd64.dylib');
-      }
-    } else if (Platform.isLinux) {
-      return DynamicLibrary.open(
-          '${Directory.current.path}/liblsl/liblsl.1.16.2-linux-amd64.so');
-    }
-    if (Platform.isAndroid) {
-      return DynamicLibrary.open(
-          '${Directory.current.path}/liblsl/liblsl.1.16.2-android-arm64-v8a.so');
-    }
-    if (Platform.isIOS) {
-      return DynamicLibrary.open(
-          '${Directory.current.path}/liblsl/liblsl.1.16.2-ios-arm64.dylib');
-    }
-    throw 'liblsl dynamic library not found';
-  }
+//   DynamicLibrary _loadLibrary() {
+//     if (Platform.isWindows) {
+//       return DynamicLibrary.open(
+//           '${Directory.current.path}/liblsl/liblsl.1.16.2-win-amd64.dll');
+//     }
+//     if (Platform.isMacOS) {
+//       if (SysInfo.kernelArchitecture == ProcessorArchitecture.arm64) {
+//         return DynamicLibrary.open(
+//             '${Directory.current.path}/liblsl/liblsl.1.16.2-osx-arm64.dylib');
+//       } else {
+//         return DynamicLibrary.open(
+//             '${Directory.current.path}/liblsl/liblsl.1.16.2-osx-amd64.dylib');
+//       }
+//     } else if (Platform.isLinux) {
+//       return DynamicLibrary.open(
+//           '${Directory.current.path}/liblsl/liblsl.1.16.2-linux-amd64.so');
+//     }
+//     if (Platform.isAndroid) {
+//       return DynamicLibrary.open(
+//           '${Directory.current.path}/liblsl/liblsl.1.16.2-android-arm64-v8a.so');
+//     }
+//     if (Platform.isIOS) {
+//       return DynamicLibrary.open(
+//           '${Directory.current.path}/liblsl/liblsl.1.16.2-ios-arm64.dylib');
+//     }
+//     throw 'liblsl dynamic library not found';
+//   }
 }
--- packages/liblsl/test/liblsl_test.dart
+++ packages/liblsl/test/liblsl_test.dart
@@ -5,19 +5,22 @@ import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final lsl = LSL();
-    setUp(() async {
-      // Additional setup goes here.
-      await lsl.createStreamInfo();
-      await lsl.createOutlet();
-    });
-
     test('Check lsl library version', () {
-      expect(lsl.version, 116);
+      expect(lsl_library_version(), 116);
     });
-    test('Wait for consumer times out with exception', () {
-      expect(lsl.waitForConsumer(timeout: 1),
-          throwsA(const TypeMatcher<TimeoutException>()));
-    });
+    // final lsl = LSL();
+    // setUp(() async {
+    //   // Additional setup goes here.
+    //   await lsl.createStreamInfo();
+    //   await lsl.createOutlet();
+    // });
+
+    // test('Check lsl library version', () {
+    //   expect(lsl.version, 116);
+    // });
+    // test('Wait for consumer times out with exception', () {
+    //   expect(lsl.waitForConsumer(timeout: 1),
+    //       throwsA(const TypeMatcher<TimeoutException>()));
+    // });
   });
 }
