3538da9b76f2866a6dda06af5c74c5ae5911b842
zeyus <dev@zeyus.com>
2025-07-01 14:03:06 +0200

    added src path in code doc to a variable.

--- packages/liblsl/hook/build.dart
+++ packages/liblsl/hook/build.dart
@@ -9,47 +9,49 @@ import 'package:native_toolchain_c/src/native_toolchain/android_ndk.dart';
 /// It is possible, but untested, to compile liblsl for WASM.
 /// Here is the command that successfully compiled it for WASM:
 /// ```bash
-/// emcc -pthread -sPTHREAD_POOL_SIZE=32 -sEXPORT_NAME=liblsl  --no-entry \
-///      -sENVIRONMENT=web,worker -DVERSION=1.16.2 -DLSL_ABI_VERSION=2 \
-///      -DASIO_NO_DEPRECATED -DBOOST_ALL_NO_LIB -DLIBLSL_EXPORTS \
-///      -DLSL_VERSION_INFO=git:x/branch:x/build:dart_native/compiler:unknown \
-///      -DLOGURU_STACKTRACES=0 -include ./src/include/lsl_lib_version.h \
-///      -I./src/liblsl-9e3823bb/lslboost -I./src/liblsl-9e3823bb/include \
-///      -I./src/liblsl-9e3823bb/thirdparty \
-///      -I./src/liblsl-9e3823bb/thirdparty/asio \
-///      -I./src/liblsl-9e3823bb/thirdparty/loguru \
-///      -I./src/liblsl-9e3823bb/thirdparty/pugixml \
-///      src/liblsl-9e3823bb/src/buildinfo.cpp \
-///      src/liblsl-9e3823bb/src/api_config.cpp \
-///      src/liblsl-9e3823bb/src/cancellation.cpp \
-///      src/liblsl-9e3823bb/src/common.cpp \
-///      src/liblsl-9e3823bb/src/consumer_queue.cpp \
-///      src/liblsl-9e3823bb/src/data_receiver.cpp \
-///      src/liblsl-9e3823bb/src/info_receiver.cpp \
-///      src/liblsl-9e3823bb/src/inlet_connection.cpp \
-///      src/liblsl-9e3823bb/src/lsl_resolver_c.cpp \
-///      src/liblsl-9e3823bb/src/lsl_inlet_c.cpp \
-///      src/liblsl-9e3823bb/src/lsl_outlet_c.cpp \
-///      src/liblsl-9e3823bb/src/lsl_streaminfo_c.cpp \
-///      src/liblsl-9e3823bb/src/lsl_xml_element_c.cpp \
-///      src/liblsl-9e3823bb/src/netinterfaces.cpp \
-///      src/liblsl-9e3823bb/src/resolver_impl.cpp \
-///      src/liblsl-9e3823bb/src/resolve_attempt_udp.cpp \
-///      src/liblsl-9e3823bb/src/sample.cpp \
-///      src/liblsl-9e3823bb/src/send_buffer.cpp \
-///      src/liblsl-9e3823bb/src/socket_utils.cpp \
-///      src/liblsl-9e3823bb/src/stream_info_impl.cpp \
-///      src/liblsl-9e3823bb/src/stream_outlet_impl.cpp \
-///      src/liblsl-9e3823bb/src/tcp_server.cpp \
-///      src/liblsl-9e3823bb/src/time_postprocessor.cpp \
-///      src/liblsl-9e3823bb/src/time_receiver.cpp \
-///      src/liblsl-9e3823bb/src/udp_server.cpp \
-///      src/liblsl-9e3823bb/src/util/cast.cpp \
-///      src/liblsl-9e3823bb/src/util/endian.cpp \
-///      src/liblsl-9e3823bb/src/util/inireader.cpp \
-///      src/liblsl-9e3823bb/src/util/strfuns.cpp \
-///      src/liblsl-9e3823bb/thirdparty/pugixml/pugixml.cpp \
-///      src/liblsl-9e3823bb/thirdparty/loguru/loguru.cpp
+/// LSL_SRC=./src/liblsl-9e3823bb
+/// emcc -pthread -sPTHREAD_POOL_SIZE=32 -sEXPORT_NAME=liblsl --no-entry \
+///       -sENVIRONMENT=web,worker -DVERSION=1.16.2 -DLSL_ABI_VERSION=2 \
+///       -DASIO_NO_DEPRECATED -DBOOST_ALL_NO_LIB -DLIBLSL_EXPORTS \
+///       -DLSL_VERSION_INFO=git:x/branch:x/build:dart_native/compiler:unknown \
+///       -DLOGURU_STACKTRACES=0 -include ./src/include/lsl_lib_version.h \
+///       -I$LSL_SRC/lslboost -I$LSL_SRC/include \
+///       -I$LSL_SRC/thirdparty \
+///       -I$LSL_SRC/thirdparty/asio \
+///       -I$LSL_SRC/thirdparty/loguru \
+///       -I$LSL_SRC/thirdparty/pugixml \
+///       $LSL_SRC/src/buildinfo.cpp \
+///       $LSL_SRC/src/api_config.cpp \
+///       $LSL_SRC/src/cancellation.cpp \
+///       $LSL_SRC/src/common.cpp \
+///       $LSL_SRC/src/consumer_queue.cpp \
+///       $LSL_SRC/src/data_receiver.cpp \
+///       $LSL_SRC/src/info_receiver.cpp \
+///       $LSL_SRC/src/inlet_connection.cpp \
+///       $LSL_SRC/src/lsl_resolver_c.cpp \
+///       $LSL_SRC/src/lsl_inlet_c.cpp \
+///       $LSL_SRC/src/lsl_outlet_c.cpp \
+///       $LSL_SRC/src/lsl_streaminfo_c.cpp \
+///       $LSL_SRC/src/lsl_xml_element_c.cpp \
+///       $LSL_SRC/src/netinterfaces.cpp \
+///       $LSL_SRC/src/resolver_impl.cpp \
+///       $LSL_SRC/src/resolve_attempt_udp.cpp \
+///       $LSL_SRC/src/sample.cpp \
+///       $LSL_SRC/src/send_buffer.cpp \
+///       $LSL_SRC/src/socket_utils.cpp \
+///       $LSL_SRC/src/stream_info_impl.cpp \
+///       $LSL_SRC/src/stream_outlet_impl.cpp \
+///       $LSL_SRC/src/tcp_server.cpp \
+///       $LSL_SRC/src/time_postprocessor.cpp \
+///       $LSL_SRC/src/time_receiver.cpp \
+///       $LSL_SRC/src/udp_server.cpp \
+///       $LSL_SRC/src/util/cast.cpp \
+///       $LSL_SRC/src/util/endian.cpp \
+///       $LSL_SRC/src/util/inireader.cpp \
+///       $LSL_SRC/src/util/strfuns.cpp \
+///       $LSL_SRC/thirdparty/pugixml/pugixml.cpp \
+///       $LSL_SRC/thirdparty/loguru/loguru.cpp \
+///       -o ./liblsl.js
 /// ```
 
 /// The default name prefix for dynamic libraries per [OS].
